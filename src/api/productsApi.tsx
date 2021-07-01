8c221fd58908e609d88f5acc23e71cd305e015ed
Edgar Olivar <edgarolivar16@gmail.com>
2021-07-01 10:04:44 -0400

    ✨ add README

--- src/api/productsApi.tsx
+++ src/api/productsApi.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const baseURL = "http://192.168.100.4:8080/api";
+const baseURL = "https://cafe-api-rn.herokuapp.com/api";
 const productsApi = axios.create({ baseURL });
 
 productsApi.interceptors.request.use(async (config) => {
